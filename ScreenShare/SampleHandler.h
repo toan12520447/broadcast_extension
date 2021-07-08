64f701ebd16e74b27a1a747ec2a6deff34616682
Toan Tran <toan.tran@softfoundry.com>
2021-07-08 14:51:18 +0700

    first commit

---
+++ ScreenShare/SampleHandler.h
@@ -0,0 +1,12 @@
+//
+//  SampleHandler.h
+//  ScreenShare
+//
+//  Created by Toan Tran on 7/7/21.
+//
+
+#import <ReplayKit/ReplayKit.h>
+
+@interface SampleHandler : RPBroadcastSampleHandler
+
+@end
---
+++ ScreenShareSetupUI/BroadcastSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  BroadcastSetupViewController.h
+//  ScreenShareSetupUI
+//
+//  Created by Toan Tran on 7/7/21.
+//
+
+#import <UIKit/UIKit.h>
+#import <ReplayKit/ReplayKit.h>
+
+@interface BroadcastSetupViewController : UIViewController
+
+@end
